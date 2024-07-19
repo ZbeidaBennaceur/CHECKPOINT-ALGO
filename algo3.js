59e2c9788644ed9bed8413cae5fc4cf0d711715f
zbeida <zbeidabennaceur@gmail.com>
2024-07-19 18:35:26 +0100

    first commit

---
+++ algo3.js
@@ -0,0 +1,48 @@
+/*const leapyear=(year) => {
+    var result=""
+
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 
+        ? `${year} is a leap year` 
+        : `${year} is not a leap year`
+
+}
+
+console.log(leapyear(2016))*/
+
+/*
+const ticket=(age) => {
+    var result =""
+    return age<=12 ? result="The price ticket is 10$"
+    : (age>=13 && age<=17) ? result="The price ticket is 15$"
+    :result="The price ticket is 20$"
+    
+}
+console.log (ticket(17))*/
+
+
+/*
+const fib=(n)=> {
+var result=0
+if ((n=1) || (n=0)) {result=1}
+else {result+=fib(n-1)}
+return result
+}
+
+*/
+
+/*
+const puissance=(n,p)=> {
+
+    if (p===0) {return 1}
+    else if (p<0) {return 1/puissance (n,-p) }
+    else {return  n * puissance (n,p-1) }
+
+}
+console.log(puissance(2,-5))
+*/
+
+
+
+
+
+
